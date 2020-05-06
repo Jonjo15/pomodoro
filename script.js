7ed7a5ad070c6b979388181bef56ae821e4374a3
IVAN <ivanbatur5g@gmail.com>
2020-05-06 15:15:32 +0200

    installed buttons for increasing/decreasing session/rest time

--- script.js
+++ script.js
@@ -1,11 +1,57 @@
 let breakTime = document.querySelector("#breakTime");
 let sessionTime = document.querySelector("#sessionTime");
 let cycle = document.querySelector("#cycle");
+let increaseSession = document.querySelector("#increaseSession");
+let decreaseSession = document.querySelector("#decreaseSession");
+let increaseBreak = document.querySelector("#increaseBreak");
+let decreaseBreak = document.querySelector("#decreaseBreak");
+let play = document.querySelector("#play");
+let pause = document.querySelector("#pause");
+let stop = document.querySelector("#stop");
+let restart = document.querySelector("#restart");
+increaseSession.addEventListener("click", (e) => {
+    if (session < 60) {
+        session += 1;
+    }
+    updateSessionTime();
+    updateClock();
+});
+decreaseSession.addEventListener("click", (e) => {
+    if (session > 5) {
+        session -= 1;
+    }
+    updateSessionTime();
+    updateClock();
+});
+increaseBreak.addEventListener("click", (e) => {
+    if (rest < 30) {
+        rest += 1;
+    }
+    updateBreakTime();
+});
+decreaseBreak.addEventListener("click", (e) => {
+    if (rest > 1) {
+        rest -= 1;
+    }
+    updateBreakTime();
+});
 console.log(breakTime, sessionTime, cycle);
-breakTime.textContent = "5 min";
-sessionTime.textContent = "25 min";
-let minutes = 25;
+let session = 25;
+let rest = 5;
+
 let seconds = "00";
+function updateBreakTime() {
+    breakTime.textContent = rest + " min";
+}
+updateBreakTime();
+function updateSessionTime() {
+    sessionTime.textContent = session + " min";
+}
+updateSessionTime();
+function updateClock() {
+    cycle.textContent = session + ":" + seconds;
+}
+updateClock();
 
 /*setInterval(function(){ cycle.textContent = minutes +":" + seconds;
 console.log(minutes +":" + seconds)
